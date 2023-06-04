37c6d5065c8c4ca02994c21891e99d34e3153439
KoynovStas <skojnov@yandex.ru>
2023-06-04 13:59:31 +0400

    new: add off_skype_telemetry.bat

---
+++ src/off_skype_telemetry.bat
@@ -0,0 +1,12 @@
+:: SPDX-License-Identifier: MIT
+@echo off
+
+set HKCU_TRACING_SKYPE=HKCU\Software\Microsoft\Tracing\WPPMediaPerApp\Skype
+
+echo Disable Skype telemetry:
+REG ADD "%HKCU_TRACING_SKYPE%\ETW" /v "TraceLevelThreshold" /t REG_DWORD /d 0 /f
+REG ADD "%HKCU_TRACING_SKYPE%"     /v "EnableTracing"       /t REG_DWORD /d 0 /f
+REG ADD "%HKCU_TRACING_SKYPE%\ETW" /v "EnableTracing"       /t REG_DWORD /d 0 /f
+REG ADD "%HKCU_TRACING_SKYPE%"     /v "WPPFilePath"         /t REG_SZ    /d "%TEMP%\Tracing\WPPMedia" /f
+REG ADD "%HKCU_TRACING_SKYPE%\ETW" /v "WPPFilePath"         /t REG_SZ    /d "%TEMP%\WPPMedia" /f
+echo:
