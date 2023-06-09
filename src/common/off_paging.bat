e94bb8585a23017470d3d1038315cb75d7809ca1
KoynovStas <skojnov@yandex.ru>
2023-06-09 19:21:33 +0400

    fix: add SETLOCAL\ENDLOCAL

--- src/common/off_paging.bat
+++ src/common/off_paging.bat
@@ -1,4 +1,5 @@
 :: SPDX-License-Identifier: MIT
+:: https://social.technet.microsoft.com/forums/windowsserver/ru-ru/708da424-3a7c-404d-8f54-f989ed62d323/command-for-changeing-the-page-file
 @echo off
 
 :: Need reboot after this tweak
--- src/common/off_skype_telemetry.bat
+++ src/common/off_skype_telemetry.bat
@@ -1,6 +1,8 @@
 :: SPDX-License-Identifier: MIT
 @echo off
 
+
+SETLOCAL
 set HKCU_TRACING_SKYPE=HKCU\Software\Microsoft\Tracing\WPPMediaPerApp\Skype
 
 echo Disable Skype telemetry:
@@ -9,4 +11,6 @@ REG ADD "%HKCU_TRACING_SKYPE%"     /v "EnableTracing"       /t REG_DWORD /d 0 /f
 REG ADD "%HKCU_TRACING_SKYPE%\ETW" /v "EnableTracing"       /t REG_DWORD /d 0 /f
 REG ADD "%HKCU_TRACING_SKYPE%"     /v "WPPFilePath"         /t REG_SZ    /d "%TEMP%\Tracing\WPPMedia" /f
 REG ADD "%HKCU_TRACING_SKYPE%\ETW" /v "WPPFilePath"         /t REG_SZ    /d "%TEMP%\WPPMedia" /f
+
 echo:
+ENDLOCAL
