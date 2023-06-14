4a52d653375ebf2476813516d3e4f7ff59a1d9e9
KoynovStas <skojnov@yandex.ru>
2023-06-14 16:17:40 +0400

    BugFix: OMG we cant use a comment along with a command

--- src/common/off_hibernate.bat
+++ src/common/off_hibernate.bat
@@ -3,6 +3,6 @@
 
 :: Need reboot after this tweak
 echo Disable Hibernate and delete hiberfil.sys:
-powercfg -h off  >nul 2>&1 rem windows 7
-powercfg /h off  >nul 2>&1 rem windows 10/11
+powercfg -h off  >nul 2>&1
+powercfg /h off  >nul 2>&1
 echo:
