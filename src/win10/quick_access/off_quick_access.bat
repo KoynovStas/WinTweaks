2f7fbe7f944f30dd0b3733c4b490d80ead7e84ff
KoynovStas <skojnov@yandex.ru>
2023-06-15 15:28:55 +0400

    new:win10: add on/off_quick_access.bat

---
+++ src/win10/quick_access/off_quick_access.bat
@@ -0,0 +1,6 @@
+:: SPDX-License-Identifier: MIT
+@echo off
+
+echo Disable Quick Access on Explorer:
+regedit /s off_quick_access.reg
+echo:
---
+++ src/win10/quick_access/on_quick_access.bat
@@ -0,0 +1,6 @@
+:: SPDX-License-Identifier: MIT
+@echo off
+
+echo Enable Quick Access on Explorer:
+regedit /s on_quick_access.reg
+echo:
